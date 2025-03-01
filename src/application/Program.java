768f266abc135446078033daa0807b76ddac3285
Gutemberg Lamark <gtmbrglamark@gmail.com>
2025-03-01 12:09:36 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,7 +42,6 @@ public class Program {
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
